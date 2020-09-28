80f73aa94ef9cc8420f8bf10cce3631d15ab9042
finallie <1006492957@qq.com>
2020-09-28 03:41:12 -0700

    4

--- main.c
+++ main.c
@@ -1,14 +1,5 @@
 #include "tlpi_hdr.h"
-#include <fcntl.h>
 
 int main() {
-    int fd = open("test.txt", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        errExit("open");
-    lseek(fd, 0, SEEK_END);
-    int n = write(fd, "abc123", 6);
-    if (n == -1)
-        errExit("write");
-    if (close(fd) == -1)
-        errExit("close");
+    printf("hello world!");
 }
